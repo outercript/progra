861d6dc366234e7fac2957605aacde8d0dfe0206
Oscar Suro <outercript@gmail.com>
2014-02-10 00:38:40 -0600

    vector: Adding some sort of unit testing
    Signed-off-by: Oscar Suro <outercript@gmail.com>

--- vector/vector.c
+++ vector/vector.c
@@ -103,4 +103,59 @@ void VectorExpand(vector *v)
     assert(v->elements != NULL);
 }
 
-void main(){}
+
+
+
+int compare_double(void *a, void *b)
+{
+    double *x = a;
+    double *y = b;
+    return *x - *y;
+}
+
+void print_double(void *elemAddr, void *auxData)
+{
+    printf("%.2f, ", *(double *)elemAddr);
+}
+
+int main()
+{
+    int i, length;
+    double d;
+    void *p;
+
+    length = 5;
+    vector test;
+    VectorNew(&test, sizeof(double), NULL, length);
+
+    // Test filling up the Vector
+    printf("Test Fill Vector\n");
+    for (i=0; i<length; i++)
+    {
+        d = 1.5*i;
+        VectorAppend(&test, &d);
+    }
+    VectorMap(&test, (VectorMapFunction)print_double, NULL);
+
+    // Replace test
+    printf("\n\nTest Replace Element\n");
+    d = 7.23;
+    VectorReplace(&test, &d, 3);
+    VectorMap(&test, (VectorMapFunction)print_double, NULL);
+
+    // Append when full test
+    printf("\n\nTest Appending to a full vector\n");
+    printf(" (Before) Vector.length = %d\n", VectorLength(&test));
+    d = 9.43;
+    VectorAppend(&test, &d);
+    printf(" (After) Vector.lenght = %d\n", VectorLength(&test));
+
+    // Test printing the vector
+    VectorMap(&test, (VectorMapFunction)print_double, NULL);
+
+    // Check sorted
+    printf("\n\nSorting the vector\n");
+    VectorSort(&test, (VectorCompareFunction)compare_double);
+    VectorMap(&test, (VectorMapFunction)print_double, NULL);
+    return 0;
+}
